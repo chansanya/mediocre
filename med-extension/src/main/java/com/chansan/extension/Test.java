9fd94341f538749f308122c05628fecaba4f0c5e
lhy <984038622@qq.com>
2022-12-15 20:28:58 +0800

    整理仓库

---
+++ med-extension/src/main/java/com/chansan/extension/Test.java
@@ -0,0 +1,12 @@
+package com.chansan.extension;
+
+/**
+ * @name: Test
+ * @author: leihuangyan
+ * @classPath: com.chansan.extension.Test
+ * @date: 2022/12/15
+ * @description:
+ */
+public class Test {
+
+}
